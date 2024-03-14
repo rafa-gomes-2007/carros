6188c0f317ff2fa04d9e9e6e83d447157d43cf5d
hick.hk <hick.hk@gmail.com>
2024-03-14 00:36:23 -0300

    setimo commit do app mobile funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,23 @@ export default function App() {
       <section>
         <div className="bg-orange p-12 rounded-t-lg">
           <img src={iconSedans} alt="iconde de carros sedans" className="mb-7"/>
-          <h2 className="font-showders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
-          <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a href="https://www.webmotors.com.br/sedans" target = "_blenk" className="font-lexend">Ver Mais</a>
+          <h2 className="font-showders mb-7 uppercase text-4xl font-bold text-offwhite-bg">Sedans</h2>
+          <p className="font-lexend text-offwhite mb-7">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/sedans" target = "_blenk" className="font-lexend bg-offwhite-bg px-10 py-3 w-fit rounded-3xl text-orange" >Ver Mais</a>
         </div>
 
         <div className="bg-green p-12"> 
           <img src={iconSUVs} alt="icone dos Suvs" className="mb-7"/>
-          <h2 className="font-showders mb-7 uppercase text-4xl font-bold text-offwhitebg">SUVs</h2>
-          <p className="font-lexend" >Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-          <a href="https://www.webmotors.com.br/suvs" target = "_blenk" className="font-lexend">Ver Mais</a>
+          <h2 className="font-showders mb-7 uppercase text-4xl font-bold text-offwhite-bg">SUVs</h2>
+          <p className="font-lexend text-offwhite mb-7" >Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/suvs" target = "_blenk" className="font-lexend bg-offwhite-bg px-10 py-3 w-fit rounded-3xl text-green">Ver Mais</a>
         </div>
 
         <div className="bg-dark-green p-12 rounded-b-lg">
           <img src={iconLuxury} alt="icone de carros de luxo" className="mb-7"/>
-          <h2 className="font-showders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
-          <p className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a href="https://www.webmotors.com.br/carros-de-luxo " target = "_blenk" className="font-lexend">Ver Mais</a>
+          <h2 className="font-showders mb-7 uppercase text-4xl font-bold text-offwhite-bg">Luxuosos</h2>
+          <p className="font-lexend text-offwhite mb-7">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-de-luxo " target = "_blenk" className="font-lexend bg-offwhite-bg px-10 py-3 w-fit rounded-3xl text-dark-green">Ver Mais</a>
         </div>
       </section>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ export default {
       'orange': '#e28625',
       'green': '#006971' ,
       'dark-green': '#004140',
-      'off-white': '#rgba(255, 255,255, 0.75)',
+      'offwhite': 'rgba(255, 255,255, 0.75)',
       'white': '#fff',
-      'off-white-bg': '#f2f2f2',
+      'offwhite-bg': '#f2f2f2',
     },
     fontFamily: {
       'showders': 'Big Shoulders Display',
