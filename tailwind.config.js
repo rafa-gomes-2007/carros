078f245e67ce90546be1deb8509b3ab094d7a68b
hick.hk <hick.hk@gmail.com>
2024-03-13 22:59:17 -0300

    instalação do tennwild

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'orange': '#e28625',
+      'green': '#006971' ,
+      'dark-green': '#004140',
+      'off-white': '#rgba(255, 255,255, 0.75)',
+      'white': '#fff',
+      'off-white-bg': '#f2f2f2',
+    }
+  },
+  plugins: [],
+
+}
